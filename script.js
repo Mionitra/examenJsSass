10d6192b4482887b0a74920d59110069f13845a3
Mionitra <rlmionitra@gmail.com>
2024-09-28 13:16:03 +0300

    javascript partie input et gestion des boutons

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Theme sombre et claire du bouton
+const keys = document.querySelectorAll('.key');
 document.querySelector('.theme').addEventListener('click', function () {
     const body = document.body;
     const calculator = document.querySelector('.calculator');
     const display = document.querySelector('.display');
-    const keys = document.querySelectorAll('.key');
 
     body.classList.toggle('light-mode');
     calculator.classList.toggle('light-mode');
@@ -15,4 +16,25 @@ document.querySelector('.theme').addEventListener('click', function () {
     } else {
         this.textContent = 'Mode clair';
     }
+});
+
+
+// Gestion des inputs du clavier
+let entree = document.querySelector('.input');
+input = "" // l'entree est initialise a vide
+keys.forEach(key => {
+    const value = key.dataset.key;
+    key.addEventListener('click', function() {
+        if (value == 'brackets') {
+            
+        if (input.split('(').length <= input.split(')').length) {
+            input += '(';
+        } else {
+            input += ')';
+        }
+        } else {
+            input += value;
+        }
+        entree.value = input;
+    });
 });
